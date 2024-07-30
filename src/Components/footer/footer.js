e5b8a2f1b077895313485b92de1c097bce6c71df
Wasiq Fayyaz <114560200+Wasiq-Fayyaz@users.noreply.github.com>
2024-07-30 16:11:59 +0500

    Update footer.js

--- src/Components/footer/footer.js
+++ src/Components/footer/footer.js
@@ -20,22 +20,12 @@ export default function Footer() {
         <small>Copyright © 2024 Mapei. All rights reserved.</small>
       </div>
       <div className="container--3">
-      <a href="https://x.com/MUFASABASE?t=95x31MFD8TMVccZnXcLvxg&s=09" target="_blank">
+      <a href="https://x.com/Mapeierc20" target="_blank">
           <FaXTwitter className="footer--logos" title="Twitter" />
         </a>
-        <a href="https://t.me/MufasaPortal" target="_blank">
+        <a href="https://t.me/Mapeierc20" target="_blank">
           <LiaTelegramPlane className="footer--logos" title="Telegram" />
         </a>
-        <a href="https://medium.com/@Mufasa_Base">
-        <FaMedium className="footer--logos" title="Medium"/>
-        </a>
-        <a href="/">
-          {" "}
-          <img src={dexTools} alt="404" className="footer--logos"></img>
-        </a>
-        {/* <a href="/">
-          <BiLogoGmail className="footer--logos" />
-        </a> */}
       </div>
     </footer>
   );
